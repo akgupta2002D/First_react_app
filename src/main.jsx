8165c75046e720bd7f38297e05c29ea00d184df9
akgupta2002D <akgupta2002d@gmail.com>
2024-10-22 04:00:13 -0400

    Learning how to use JSX

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-import Greetings from "./Greetings.jsx";
-import Testing from "./Testing.jsx";
+import Greetings from "./components/Greetings.jsx";
+import Testing from "./components/Testing.jsx";
+import Avatar from './components/Avatar.jsx';
+import TodoList from './components/TodoList.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greetings />
+    <Avatar/>
     <Testing/>
+    <TodoList/>
   </StrictMode>,
 )
